42a05b661c6b83054bcf899f58aba761b0f179db
sevase <kotaseva8@gmail.com>
2023-06-12 01:10:51 +0300

    написан поток для получения постоянных сообщений

--- clients/client1.py
+++ clients/client1.py
@@ -16,6 +16,7 @@ PORT = 12345'''
 import socket
 from tkinter import *
 from tkinter import scrolledtext
+import threading
 
 
 # хост и порт
@@ -56,14 +57,29 @@ btn.pack()
 
 
 
+# функция получения сообщений
+def out_message():
+    while True:
+        # обновление интерфейса
+        window.update()
 
+        # получение и вывод сообщений
+        try:
+            mes = client1_sock.recv(1024).decode('utf-8')
 
+            if mes:
+                message_display.insert(END, f'{mes}\n')
+        except socket.error as e:
+            print(f'Ошибка: {e}')
+            break
 
+# поток для получения беспрерывных соо
+mes_theard = threading.Thread(target=out_message)
 
+'''как я понял, что этот поток будет автоматически остановлен, когда основной 
+поток программы (в данном случае window.mainloop()) завершится. Это позволяет корректно завершать 
+программу при закрытии окна.'''
+mes_theard.daemon = True
+mes_theard.start() # запуск потока
 
-
-flag = True
-
-while flag:
-    mes = client1_sock.recv(1024).decode('utf-8')
-    print(f'Клиент2 - {mes}\n')
+window.mainloop()
