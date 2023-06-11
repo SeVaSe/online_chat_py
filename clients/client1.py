5ead37a2f5f3ef135f6d15b322f422dbbd8f047c
sevase <kotaseva8@gmail.com>
2023-06-12 00:38:48 +0300

    сделал кнопку, ввод текста и отправку(интерфейсом)

--- clients/client1.py
+++ clients/client1.py
@@ -15,22 +15,49 @@ PORT = 12345'''
 
 import socket
 from tkinter import *
+from tkinter import scrolledtext
 
 
+# хост и порт
+HOST = 'localhost'
+PORT = 12345
+
+# установление данных сокету
+client1_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client1_sock.connect((HOST, PORT))
+print(f'Присоеденился к серверу - [{HOST}]')
+
+
+
+### TKINTER часть
 # окно
 window = Tk()
 window.title('Онлайн чат. Клиент1')
 window.geometry('400x600')
 
+# отображение текста
+message_display = scrolledtext.ScrolledText(window)
+message_display.pack()
+
+# ввод текста
+input_text = Entry(window)
+input_text.pack()
+
+# функция отправки соо
+def send_message_click():
+    message = input_text.get()
+
+    if message != '':       #если сообщение не равно пустому символу
+        client1_sock.sendall(message.encode('utf-8'))
+
+# кнопка "отправить"
+btn = Button(window, text='Отправить', command=send_message_click)
+btn.pack()
 
 
 
-HOST = 'localhost'
-PORT = 12345
 
-client1_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client1_sock.connect((HOST, PORT))
-print(f'Присоеденился к серверу - [{HOST}]')
+
 
 
 
@@ -38,10 +65,5 @@ print(f'Присоеденился к серверу - [{HOST}]')
 flag = True
 
 while flag:
-    message = input('Введите сообщение: ')       #вводим сообщение
-
-    if message != '':       #если сообщение не равно пустому символу
-        client1_sock.sendall(message.encode('utf-8'))
-
     mes = client1_sock.recv(1024).decode('utf-8')
     print(f'Клиент2 - {mes}\n')
